282520e0806802acf2e646bc57ba51185d2b6484
flynnlabexeter <s.m.flynn@exeter.ac.uk>
2025-02-14 21:59:35 +0000

    Create 2_Read2.sh

---
+++ 2_Read2.sh
@@ -0,0 +1,7 @@
+Follow steps in, but process only R2 data. 
+
+https://github.com/flynnlabexeter/CUT-Tag/blob/main/2_trim.sh
+
+https://github.com/flynnlabexeter/CUT-Tag/blob/main/3_align.sh
+
+https://github.com/flynnlabexeter/CUT-Tag/blob/main/4_samtobigwig.sh
